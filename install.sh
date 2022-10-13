3ec9b7c48bbfaa82b783d0acc7b8d84293fe17fd
BreadTeleporter <103989916+BreadTeleporter@users.noreply.github.com>
2022-10-13 13:28:58 +0100

    i forgor to give execute to the script 💀

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ echo Please wait, cloning repo...
 git clone https://github.com/BreadTeleporter/SimpleAURHelper.git
 cd SimpleAURHelper
 sudo cp aur /bin/
+chmod o+rx /bin/aur
 echo Done! Run this script again to update.
