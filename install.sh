94c299b5fa9175deae0ca4357702d22b7f925876
BreadTeleporter <103989916+BreadTeleporter@users.noreply.github.com>
2022-10-21 14:39:31 +0100

    Added updater

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ echo Please wait, cloning repo...
 git clone https://github.com/BreadTeleporter/SimpleAURHelper.git
 cd SimpleAURHelper
 sudo cp aur /bin/
+sudo cp aur-update /bin/
 chmod o+rx /bin/aur
 echo Done! Run this script again to update.
