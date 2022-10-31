4e18d5bfce813770bbb9126d16807cca801cedb9
BreadTeleporter <103989916+BreadTeleporter@users.noreply.github.com>
2022-10-31 13:53:19 +0000

    Install to /usr/bin instead of /bin

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ sudo pacman -S git
 echo Please wait, cloning repo...
 git clone https://github.com/BreadTeleporter/SimpleAURHelper.git
 cd SimpleAURHelper
-sudo cp aur /bin/
-sudo cp aur-update /bin/
-chmod o+rx /bin/aur
-chmod o+rx /bin/aur-update
+sudo cp aur /usr/bin/
+sudo cp aur-update /usr/bin/
+chmod o+rx /usr/bin/aur
+chmod o+rx /usr/bin/aur-update
 echo Done! Run this script again to update.
